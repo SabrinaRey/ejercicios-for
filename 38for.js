77adcc2e5322228ce7dec556267d33039e7e1fd9
/SabrinaRey <sabrinareynares@gmail.com>
2019-11-06 18:43:02 -0300

    primer commit

---
+++ 38for.js
@@ -0,0 +1,15 @@
+for (let i = 0; i <= 10; i++ ) {
+    console.log(i)
+};
+
+for (let i = 100; i >= 0; i-- ) {
+    console.log(i)
+};
+
+for (let i = 0; i <= 1000; i+=2 ) {
+    console.log(i)
+};
+
+for (let i = 0; i <= 1000; i+=2 ) {
+    console.log(i)
+};
